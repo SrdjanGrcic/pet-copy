637a1becd019b969462b42d44fe3b4c44778d9f3
SrdjanGrcic <srdjangrs@gmail.com>
2016-12-30 09:34:29 +0100

    First commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,13 @@
+/*!
+ * web-application
+ * 
+ * 
+ * @author Jovica Čonkič
+ * @version 1.0.0
+ * Copyright . ISC licensed.
+ */
+(function ($, window, document) {
+
+  'use strict';
+  
+})(jQuery, window, document);
---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+(function ($, window, document) {
+
+  'use strict';
+  
+})(jQuery, window, document);
